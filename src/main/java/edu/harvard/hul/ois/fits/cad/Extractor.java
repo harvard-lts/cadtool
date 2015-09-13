b4b8b3f8f4df7441603355ac77cd94b811724bf6
Isaac Simmons <ids23@drexel.edu>
2015-09-12 23:34:56 -0400

    Expose set of supported extensions from extractor. Rename base result XML element

--- src/main/java/edu/harvard/hul/ois/fits/cad/Extractor.java
+++ src/main/java/edu/harvard/hul/ois/fits/cad/Extractor.java
@@ -4,25 +4,28 @@ import org.jdom.Element;
 
 import javax.activation.DataSource;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public abstract class Extractor {
-    private final Set<String> extensions = new HashSet<>();
+    private final Set<String> extensions;
     private final String name;
 
     protected Extractor(String name, String... extensions) {
         this.name = name;
 
         //Make sure every extension is lowercase and prefixed by a period
+        final Set<String> temp = new HashSet<>(extensions.length);
         for(String extension: extensions) {
             extension = extension.toLowerCase();
             if (extension.charAt(0) == '.') {
-                this.extensions.add(extension);
+                temp.add(extension);
             } else {
-                this.extensions.add("." + extension);
+                temp.add("." + extension);
             }
         }
+        this.extensions = Collections.unmodifiableSet(temp);
     }
 
     public boolean accepts(String filename) {
@@ -38,7 +41,7 @@ public abstract class Extractor {
     protected abstract void doRun(DataSource ds, String filename, Element result) throws IOException;
 
     public final Element run(DataSource ds, String filename) throws IOException {
-        final Element result = new Element("result");
+        final Element result = new Element("cad-tool-result");
         result.setAttribute("extractor", name);
         result.setAttribute("file", filename);
         doRun(ds, filename, result);
@@ -47,5 +50,9 @@ public abstract class Extractor {
 
     public String getName() {
         return name;
+    }
+
+    public Set<String> getExtensions() {
+        return extensions;
     }
 }
