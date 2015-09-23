84d4f9ad618f80273899794e83dcd7130c132186
Isaac Simmons <ids23@drexel.edu>
2015-09-23 12:25:09 -0400

    MagicNumberValidator option to leave streams open after validating

--- src/main/java/edu/harvard/hul/ois/fits/cad/MagicNumberValidator.java
+++ src/main/java/edu/harvard/hul/ois/fits/cad/MagicNumberValidator.java
@@ -12,55 +12,78 @@ import java.util.Arrays;
  */
 public class MagicNumberValidator {
     private final byte[] magic;
+    private final boolean closeStream;
 
-    private MagicNumberValidator(byte[] magic) {
+    private MagicNumberValidator(byte[] magic, boolean closeStream) {
         this.magic = magic;
+        this.closeStream = closeStream;
     }
 
-    public static MagicNumberValidator hex(String hex) {
+    public static MagicNumberValidator hex(String hex, boolean closeStream) {
         if (hex.startsWith("0x")) {
             hex = hex.substring(2);
         }
 
-        return new MagicNumberValidator(DatatypeConverter.parseHexBinary(hex));
+        return new MagicNumberValidator(DatatypeConverter.parseHexBinary(hex), closeStream);
     }
 
-    public static MagicNumberValidator string(String s) {
+    public static MagicNumberValidator hex(String hex) {
+        return MagicNumberValidator.hex(hex, true);
+    }
+
+    public static MagicNumberValidator string(String s, boolean closeStream) {
         try {
-            return MagicNumberValidator.string(s, Charset.defaultCharset().name());
+            return MagicNumberValidator.string(s, Charset.defaultCharset().name(), closeStream);
         } catch(UnsupportedEncodingException ex) {
             throw new RuntimeException("System default charset not found", ex);
         }
     }
 
+    public static MagicNumberValidator string(String s) {
+        return MagicNumberValidator.string(s, true);
+    }
+
+    public static MagicNumberValidator string(String s, String charset, boolean closeStream) throws UnsupportedEncodingException {
+        return new MagicNumberValidator(s.getBytes(charset), closeStream);
+    }
+
     public static MagicNumberValidator string(String s, String charset) throws UnsupportedEncodingException {
-        return new MagicNumberValidator(s.getBytes(charset));
+        return MagicNumberValidator.string(s, charset, true);
+    }
+
+    public static MagicNumberValidator bytes(byte[] bytes, boolean closeStream) {
+        return new MagicNumberValidator(bytes, closeStream);
     }
 
     public static MagicNumberValidator bytes(byte[] bytes) {
-        return new MagicNumberValidator(bytes);
+        return MagicNumberValidator.bytes(bytes, true);
+    }
+
+    public static byte[] readBytes(InputStream in, int length) throws IOException {
+        final byte[] buf = new byte[length];
+
+        int offset = 0;
+
+        while(offset < length) {
+            final int read = in.read(buf, offset, length - offset);
+            if (read == -1) {
+                throw new IOException("Reached end of stream before reading expected number of bytes");
+            }
+            offset += read;
+        }
+        return buf;
     }
 
     public boolean isValid(InputStream in) throws IOException {
         //TODO: might I get something with a UTF-8 (or other) BOM?
         try {
-            final byte[] buf = new byte[magic.length];
-
-            int offset = 0;
-
-            while(offset < magic.length) {
-                final int read = in.read(buf, offset, magic.length - offset);
-                if (read == -1) {
-                    return false;
-                }
-                offset += read;
-            }
-
-            return Arrays.equals(buf, magic);
+            return Arrays.equals(readBytes(in, magic.length), magic);
         } finally {
-            try {
-                in.close();
-            } catch (IOException ignored) {}
+            if (closeStream) {
+                try {
+                    in.close();
+                } catch (IOException ignored) {}
+            }
         }
     }
 
