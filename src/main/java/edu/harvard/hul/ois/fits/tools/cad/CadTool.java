4389dc431435509a5614af73944539ff8404d3c2
Isaac Simmons <ids23@drexel.edu>
2015-10-01 16:28:29 -0400

    Update version numbers

--- src/main/java/edu/harvard/hul/ois/fits/tools/cad/CadTool.java
+++ src/main/java/edu/harvard/hul/ois/fits/tools/cad/CadTool.java
@@ -19,7 +19,7 @@ import java.nio.file.Files;
 import java.util.*;
 
 public class CadTool extends ToolBase {
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "1.0";
     public static final String NAME = "cadtool";
 
     public static final String PREFERRED_DATE_FORMAT = "yyyy-MM-dd'T'HH:mmX";
